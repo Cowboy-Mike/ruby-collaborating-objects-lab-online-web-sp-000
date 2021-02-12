266de0820e9312a6ab077ab79394b3ac6ebe3abf
Mike Nanto <mike@thecowboymechanic.com>
2021-02-12 07:19:03 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,5 +1,5 @@
 class Artist 
-  attr_accessor :name 
+  attr_accessor :artist 
   
   @@all = []
   
@@ -8,7 +8,7 @@ class Artist
   end 
   
   def initialize(name)
-    @name = name 
+    @artist = name 
     @@all << self 
   
   end 
