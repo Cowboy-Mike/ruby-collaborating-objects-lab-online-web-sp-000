44defab778f400e2d49ceea9153e18034b58d0a3
Mike Nanto <mike@thecowboymechanic.com>
2021-02-12 08:03:57 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -12,9 +12,10 @@ class Song
     @@all << self 
   end 
   
-  def artist_name
-    artist. if artist
-  end 
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by_name(name)
+    artist.add_song(self)
+  end
   
   def self.new_by_filename(name)
     song = name.split(" - ")[1]
